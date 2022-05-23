8a28583ccb7bca83fb2e56d755cf89c203f80a07
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-05-23 09:00:52 -0700

    implement navbar

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,11 +3,11 @@ import background from '../../images/banner/hero-banner.jpg'
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(${background})`}}>
-            <div class="hero-content text-center">
-                <div class="max-w-5xl">
-                    <h1 class="text-5xl font-bold">Car Parts Manufacturer</h1>
-                    <p class="py-6">A category is a way of classifying a part in inventory. The category appears as a one-letter abbreviation under Part Detail.</p>
+        <div className="hero min-h-screen bg-center bg-no-repeat bg-cover" style={{backgroundImage: `url(${background})`}}>
+            <div className="hero-content text-center">
+                <div className="max-w-5xl">
+                    <h1 className="text-5xl font-bold">Car Parts Manufacturer</h1>
+                    <p className="py-6">A category is a way of classifying a part in inventory. The category appears as a one-letter abbreviation under Part Detail.</p>
                 </div>
             </div>
         </div>
